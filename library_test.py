79c265cc95c16c12d835893a85729136156987d6
AlexCurl <144244712+AlexanderKud@users.noreply.github.com>
2025-09-18 21:21:16 +0300

    Update library_test.py

--- library_test.py
+++ library_test.py
@@ -5,9 +5,16 @@ P = secp256k1.scalar_multiplication(pk)
 print(P.hex())
 print(secp256k1.point_to_upub(P)) # uncompressed pubkey
 print(secp256k1.point_to_cpub(P)) # compressed pubkey
-print(secp256k1.privatekey_to_hash160(0, False, pk)) # uncompressed
-print(secp256k1.privatekey_to_hash160(0, True, pk))  # compressed
+'''
+ripemd160 hash
+P2PKH  0
+P2SH   1
+BECH32 2
+'''
+print(secp256k1.privatekey_to_hash160(0, False, pk)) # uncompressed ripemd160 hash
+print(secp256k1.privatekey_to_hash160(0, True, pk))  # compressed ripemd160 hash
 print()
 Q = secp256k1.scalar_multiplication(33)
-print(secp256k1.publickey_to_hash160(0, False, Q)) # uncompressed
-print(secp256k1.publickey_to_hash160(0, True, Q))  # compressed
+print(secp256k1.publickey_to_hash160(0, False, Q)) # uncompressed ripemd160 hash
+print(secp256k1.publickey_to_hash160(0, True, Q))  # compressed ripemd160 hash
+
