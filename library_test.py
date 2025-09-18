3d3eccc0d13b41b3068722a665f59e2a1829fc99
AlexCurl <144244712+AlexanderKud@users.noreply.github.com>
2025-09-18 21:14:51 +0300

    Update library_test.py

--- library_test.py
+++ library_test.py
@@ -3,11 +3,11 @@ import secp256k1
 pk = 115792089237316195423570985008687907852837564279074904382605163141518161494336
 P = secp256k1.scalar_multiplication(pk)
 print(P.hex())
-print(secp256k1.point_to_upub(P))
-print(secp256k1.point_to_cpub(P))
-print(secp256k1.privatekey_to_hash160(0, False, pk))
-print(secp256k1.privatekey_to_hash160(0, True, pk))
+print(secp256k1.point_to_upub(P)) # uncompressed pubkey
+print(secp256k1.point_to_cpub(P)) # compressed pubkey
+print(secp256k1.privatekey_to_hash160(0, False, pk)) # uncompressed
+print(secp256k1.privatekey_to_hash160(0, True, pk))  # compressed
 print()
 Q = secp256k1.scalar_multiplication(33)
-print(secp256k1.publickey_to_hash160(0, False, Q))
-print(secp256k1.publickey_to_hash160(0, True, Q))
+print(secp256k1.publickey_to_hash160(0, False, Q)) # uncompressed
+print(secp256k1.publickey_to_hash160(0, True, Q))  # compressed
